ca84b94f342c04dd23bf10f3fd8edc661ce87af2
Giibe187 <giibewrb05@gmail.com>
2022-11-05 05:39:37 +0700

    Program Mesin Buah

---
+++ Contoh.py
@@ -0,0 +1,18 @@
+print ("=" * 25)
+print ("MESIN BUAH")
+print ("1 Buah Manggis")
+print ("2 Buah Mangga")
+print ("3 Buah Pepaya")
+print ("=" * 25)
+
+Pilihan = input('Pilih menu :')
+
+if Pilihan == '1':
+    print("Buah Manggis harga Rp.20000")
+elif Pilihan == '2':
+    print("Buah Mangga harga Rp.25000")
+elif Pilihan == '3':
+    print("Buah Pepaya harga Rp.30000")
+else:
+    print("Pilihan tidak sesuai.")
+    print("Masukan pilihan dengan benar!")
